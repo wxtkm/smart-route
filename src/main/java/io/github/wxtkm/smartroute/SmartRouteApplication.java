72c7e560eff042fea650aa33a2756ea3b7c5514f
taskmanager <dimakovtun277@gmail.com>
2026-07-15 17:08:09 +0200

    chore: initialize SmartRoute project

---
+++ src/main/java/io/github/wxtkm/smartroute/SmartRouteApplication.java
@@ -0,0 +1,13 @@
+package io.github.wxtkm.smartroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wxtkm/smartroute/SmartRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wxtkm.smartroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
